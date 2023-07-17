1eca54696e4d37e2a27286c50af887f2da017b5f
juanpaablo <pablosalazar1@outlook.com>
2023-07-16 21:08:22 -0300

    creee un carrusel de forma manual y toca agregarle funcionalidad con la bd

--- src/components/carrusel.js
+++ src/components/carrusel.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../styles/carrusel.css"
-function carrusel ({imagenes}){
-  return  <div className="container" >
-    <button> atras </button>
-    {imagenes.map((imagen, index) =>{
-      return <img key={index} src={imagen} alt="imagen" ></img>
-    })}
-    <button> adelante </button>
-  </div>
+import "../styles/carrusel.css";
+import { useSearchParams } from "react-router-dom";
 
+function Carrusel({ imagenes }) { 
+  const [ImagenActual, setImagenActual] = useState(0);
+  const cantidad = imagenes?.length;
+
+  if (!Array.isArray(imagenes) || cantidad === 0) {
+    return <div>No hay imágenes disponibles.</div>;
+  }
+
+  const siguienteimagen = () => {
+    setImagenActual((prevImagenActual) => (prevImagenActual + 1) % cantidad);
+  };
+
+  const anteriorimagen = () => {
+    setImagenActual((prevImagenActual) => (prevImagenActual - 1 + cantidad) % cantidad);
+  };
+console.log(ImagenActual)
+  return (
+    <div className="container">
+      <button onClick={anteriorimagen}>atras</button>
+      {imagenes.map((imagen, index) => (
+        <div className={ImagenActual === index ? `${Carrusel.slide} ${Carrusel.active}` : Carrusel.slide} key={index}>
+          {ImagenActual === index && <img src={imagen} alt="imagen actual" />}
+        </div>
+      ))}
+      <button onClick={siguienteimagen}>adelante</button>
+    </div>
+  );
 }
-export default carrusel;
+
+export default Carrusel;
