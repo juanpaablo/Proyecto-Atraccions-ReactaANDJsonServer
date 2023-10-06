944abb6b3f77c35a062ab3e783ec575c0ce5471a
juanpaablo <pablosalazar1@outlook.com>
2023-10-06 16:36:45 -0300

    se sigue mejorando el apartado para agregar locales

--- src/components/locales.js
+++ src/components/locales.js
@@ -1,67 +1,109 @@
-import React, { useState } from "react";
-import {Link} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import "../styles/atraccion.css"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-function Locales (){
- const url= "http://localhost:3005/locales"
- const [NewLocal, setNewLocal] = useState({
-   name:"",
-   direccion:"",
-   img:"",
-   pais:"",
-   referencias:""
- });
- //el e.target.name hace referencia a la propiedad del input del html basicamente captura lo que haya en ese input con el nombre y lo pasa a la database
-const handleChange= (e) =>{
-setNewLocal({
-    ...NewLocal,
-    [e.target.name]: e.target.value,
-})}
-const addAtraccion = async (e) =>{
-    e.preventDefault();
-    if (NewLocal.name === "" || NewLocal.direccion === "" || NewLocal.img === ""|| NewLocal.pais === "") {
-      toast.error("Por favor, complete todos los campos de imágenes.");
-      return
-    }
-const response = await axios.post(url,NewLocal);
-console.log(response);
-if(response.status === 201){
-    toast.success(NewLocal.name + " se agrego correctamente ")
-    resetform()
-} else{
-    toast.error("error al crear una nueva atraccion")
-    toast.error("intente nuevamente mas tarde")
-}
+import "../styles/atraccion.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-}
-const resetform =() =>{
+function Locales() {
+  const url = "http://localhost:3005/locales";
+  const urlatraccion = "http://localhost:3005/atracciones";
+  const [NewLocal, setNewLocal] = useState({
+    name: "",
+    direccion: "",
+    img: "",
+    pais: "",
+    referencias: "",
+    id:""
+  });
+  const [listatraccion, setlistatraccion] = useState([]);
+  const [selectedAtraccion, setSelectedAtraccion] = useState({});
+  const [selectedoption, setselectoption] = useState ("")
+
+  const Getlist = async () => {
+    const response = await axios.get(urlatraccion);
+    return response;
+  };
+
+  const handleChange = (e) => {
     setNewLocal({
-        name:"",
-        direccion:"",
-        img:"",
-        pais:"",
-        referencias:""
+      ...NewLocal,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleselect = (e) => {
+    const selectedAtraccionName = e.target.value;
+    const selectedAtraccion = listatraccion.find(
+      (atraccion) => atraccion.name === selectedAtraccionName
+    );
+    setSelectedAtraccion(selectedAtraccion)
+    setNewLocal({
+      ...NewLocal,
+      referencias:selectedAtraccionName
     })
-}
-return (
+    setselectoption(selectedAtraccionName)
+  };
+console.log(selectedAtraccion)
+  const addAtraccion = async (e) => {
+    e.preventDefault();
+    if (
+      NewLocal.name === "" ||
+      NewLocal.direccion === "" ||
+      NewLocal.img === "" ||
+      NewLocal.pais === ""
+    ) {
+      toast.error("Por favor, complete todos los campos de imágenes.");
+      return;
+    }
+    const response = await axios.post(url, NewLocal);
+    console.log(response);
+    if (response.status === 201) {
+      toast.success(NewLocal.name + " se agregó correctamente ");
+      resetform();
+    } else {
+      toast.error("Error al crear una nueva atracción");
+      toast.error("Intente nuevamente más tarde");
+    }
+  };
+
+  const resetform = () => {
+    setNewLocal({
+      name: "",
+      direccion: "",
+      img: "",
+      pais: "",
+      referencias: ""
+    });
+    setSelectedAtraccion("");
+    setselectoption("")
+
+  };
+  
+console.log(selectedoption)
+  useEffect(() => {
+    Getlist().then((response) => {
+      setlistatraccion(response.data);
+    });
+  }, []);
+  return (
     <div className="container-atraccion">
       <form className="form-insert-atraccion" onSubmit={addAtraccion}>
         <div className="form-group">
           <label className="label-atraccion"> Name: </label>
           <input
             className="input"
-            placeholder="insert the name of the local"
+            placeholder="Insert the name of the local"
             type="text"
             name="name"
             onChange={handleChange}
             value={NewLocal.name}
             required
+            id="name"
           />
         </div>
         <div className="form-group">
-          <label className="label-atraccion"> direccion: </label>
+          <label className="label-atraccion"> Direccion: </label>
           <input
             className="input"
             placeholder="Insert direccion"
@@ -69,64 +111,75 @@ return (
             name="direccion"
             value={NewLocal.direccion}
             onChange={handleChange}
+            id="direccion"
           />
         </div>
         <div className="form-group">
-          <label className="label-atraccion"> image: </label>
+          <label className="label-atraccion"> Image: </label>
           <input
             className="input"
-            placeholder="insert image url"
+            placeholder="Insert image url"
             type="text"
-            //hace referencia a esto
             name="img"
             value={NewLocal.img}
             onChange={handleChange}
+            id="img"
           />
         </div>
         <div className="form-group">
-          <label className="label-atraccion"> referencias: </label>
+          <label className="label-atraccion"> Seleccionar atracción: </label>
+          <select
+            className="input"
+            name="atraccion"
+            onChange={handleselect}
+            value={selectedoption}
+          >
+            <option value="">Seleccionar atracción</option>
+            {listatraccion.map((atraccion) => (
+              <option key={atraccion.id} value={atraccion.name}>
+                {atraccion.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label className="label-atraccion"> Referencias: </label>
           <input
             className="input"
-            placeholder="insert referencias"
+            placeholder="Insert referencias"
             type="text"
-            //hace referencia a esto
             name="referencias"
             value={NewLocal.referencias}
             onChange={handleChange}
+            id="referencias"
           />
         </div>
         <div className="form-group">
-          <label className="label-atraccion"> pais: </label>
+          <label className="label-atraccion"> Pais: </label>
           <input
             className="input"
-            placeholder="insert el pais del local"
+            placeholder="Insert el pais del local"
             type="text"
-            //hace referencia a esto
             name="pais"
             value={NewLocal.pais}
             onChange={handleChange}
+            id="pais"
           />
         </div>
 
         <button type="submit" className="button">
           ADD
         </button>
-        <br/> 
-        <br/> 
-        
-        
-        <ToastContainer/>
-        <Link to="/"> <button className="simple" > ver las atrraciones registradas</button> </Link>
+        <br />
+        <br />
+
+        <ToastContainer />
+        <Link to="/">
+          <button className="simple">Ver las atracciones registradas</button>
+        </Link>
       </form>
-
-      
-      
     </div>
-    
   );
-
-
 }
-
 
 export default Locales;
